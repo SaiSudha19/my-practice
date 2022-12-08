5e5b81bc6a0aa5b1be1cce5eb822d99074fe5707
SaiSudha19 <93865988+SaiSudha19@users.noreply.github.com>
2022-12-08 12:01:39 +0530

    Create welcome_message.cpp

---
+++ welcome_message.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout <<"enter your name\n" ;
+    getline(cin,name);
+    
+    cout<<"welcome Mr/Miss."<<name;
+    
+
+    return 0;
+}
