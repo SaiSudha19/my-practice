ef5e1b26f5e1274d13228c30c7a2fd2f1cdb30d7
SaiSudha19 <93865988+SaiSudha19@users.noreply.github.com>
2022-12-08 12:03:28 +0530

    Create sum_of_n.cpp

---
+++ sum_of_n.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,a;
+    cout<<"enter n:";
+    cin>>n;
+    a=(n*(n+1))/2;
+    cout<<"sum of first " <<n << " natural numbers are:"<<a;
+    return 0;
+}
