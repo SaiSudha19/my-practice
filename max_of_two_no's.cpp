8ee259bb871af69855738a207b93fdb79210c10c
SaiSudha19 <93865988+SaiSudha19@users.noreply.github.com>
2022-12-08 16:58:41 +0530

    Create max_of_two_no's.cpp

---
+++ max_of_two_no's.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"enter two numbers\n";
+    cin >> a>>b;
+    cout<<"max of two mumbers is:";
+    if(a>b)
+    {
+        cout<<a;
+    }
+    else
+    {
+        cout<<b;
+    }
+    return 0;
+}
