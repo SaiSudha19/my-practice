b449fbed594dbf6463ff6c333027fb373db02712
SaiSudha19 <93865988+SaiSudha19@users.noreply.github.com>
2022-12-08 16:52:12 +0530

    Create coumpound_assignment.cpp

---
+++ coumpound_assignment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+   int a=2,b=3;
+   int sum=4;
+   int prod=3;
+   sum+=a+b;
+    prod*=a*b;
+   
+   std::cout << sum << std::endl;
+   std::cout <<prod  << std::endl;
+
+    return 0; 
+}
+
