4e39198867b131fcfa016742e7084dcbbaffc55c
SaiSudha19 <93865988+SaiSudha19@users.noreply.github.com>
2022-12-09 11:41:12 +0530

    Create working_hour.cpp

---
+++ working_hour.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"enter hour:";
+    cin>>a;
+    if(a>=9&&a<=18)
+    {
+        cout<<"it's an working hour...enjoy in hell";
+    }
+    else
+    {
+        cout<<"it's not an working hour...have a grat time";
+    }
+    
+
+    return 0;
+}
