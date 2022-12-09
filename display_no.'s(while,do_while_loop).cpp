eb6880d87d24410ee5a15cad7ece3a52a139c4fb
SaiSudha19 <93865988+SaiSudha19@users.noreply.github.com>
2022-12-09 11:49:45 +0530

    Create display_no.'s(while,do_while_loop).cpp

---
+++ display_no.'s(while,do_while_loop).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    cout<<endl;
+    int k=100;
+    do
+    {
+        cout<<k<<endl;
+        k++;
+    }while(k<=110);
+
+    return 0;
+}
