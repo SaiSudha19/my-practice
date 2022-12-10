c68c3b6e709291d684651bb98a709c1ac187f816
SaiSudha19 <93865988+SaiSudha19@users.noreply.github.com>
2022-12-10 08:44:57 +0530

    Create length_of_string.cpp

---
+++ length_of_string.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+   char s[]="hello world";
+   /*
+   char *s;
+   cout<<"enter the string";
+   cin>>s;// or cin.getine(s,100);
+   */
+   cout<<strlen(s)<<endl;
+
+    return 0;
+}
