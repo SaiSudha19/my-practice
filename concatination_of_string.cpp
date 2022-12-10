32548511798e35c6987c1473e82b6bbb698b29e7
SaiSudha19 <93865988+SaiSudha19@users.noreply.github.com>
2022-12-10 08:45:43 +0530

    Create concatination_of_string.cpp

---
+++ concatination_of_string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+   char s1[20]="good";
+   char s2[10]="morning";
+   cout << strcat(s1,s2)<<endl;
+   //cout<< strncat(s1,s2,3);
+    return 0;
+}
