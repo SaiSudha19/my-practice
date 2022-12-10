227e0ec55e9d2e4e4e511846fea726abc885754b
SaiSudha19 <93865988+SaiSudha19@users.noreply.github.com>
2022-12-10 08:39:11 +0530

    Create pointer_arthematic.cpp

---
+++ pointer_arthematic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int A[5]{1,2,3,4,5};
+    int *p=A;
+    cout<<*p<<endl;
+    p++;
+    cout<<*p<<endl;
+    p--;
+    cout<<*p<<endl;
+    cout<<p+2<<endl;
+    cout<<*(p+2)<<endl;
+}
