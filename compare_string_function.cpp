fa4a3a79ed78c44f1f8f678a82fbfda80a55fe70
SaiSudha19 <93865988+SaiSudha19@users.noreply.github.com>
2022-12-10 08:49:24 +0530

    Create compare_string_function.cpp

---
+++ compare_string_function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char s1[20];
+    char s2[20];
+    cout<<"enter s1:";
+    cin>>s1;
+    cout<<"enter s2:";
+    cin>>s2;
+    cout<<strcmp(s1,s2);
+    return 0;
+}
