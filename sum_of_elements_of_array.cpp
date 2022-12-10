4c5524e4b2c939c5ece9674505592d589b8cc4b1
SaiSudha19 <93865988+SaiSudha19@users.noreply.github.com>
2022-12-10 08:19:45 +0530

    Create sum_of_elements_of_array.cpp

---
+++ sum_of_elements_of_array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,sum=0, A[]={2,4,6,8,10};
+    for(i=0;i<5;i++)
+    {
+        sum=sum+A[i];
+        
+    }
+    cout<<sum;
+return 0;
+    
+}
