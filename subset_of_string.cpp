67055a0c1810e5e55399ebfaeed37f9327ed71af
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2022-12-19 23:45:08 +0530

    Create subset_of_string.cpp

---
+++ subset_of_string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+
+{
+    string s="programming";
+    cout<<s.substr(3)<<endl;
+    cout<<s.substr(3,4)<<endl;
+    return 0;
+
+}
