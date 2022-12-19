874de066a04a053736d68e952ddda09960d20c5f
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2022-12-19 23:45:44 +0530

    Create comparing_strings.cpp

---
+++ comparing_strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+
+{
+    string s="programming";
+    string s1="programming";
+    string s2="abcd";
+    cout<<s.compare(s1)<<endl;
+    cout<<s.compare(s2)<<endl;
+    return 0;
+
+}
